c4864c65389392eb956e4b36c34bdd60b89ebd02
vbogdev <vbogdev@ucsd.edu>
2022-02-25 00:09:36 -0800

    Week 8 lab

--- MarkdownParseTestGroup.java
+++ MarkdownParseTestGroup.java
@@ -1,65 +1,40 @@
-import static org.junit.Assert.assertEquals;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
-import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import org.junit.*;
+
 public class MarkdownParseTestGroup {
-    /**@Test
-    public void testEmpty() throws IOException {
-        assertLinks(List.of(), "emptyGroup.md");
-    }
-    */
     @Test
-    public void testExtraSpace() throws IOException {
-        assertLinks(List.of(), "extraSpaceGroup.md");
-    }
-
-    @Test
-    public void testEscape() throws IOException {
-        assertLinks(List.of("https://somethingelse.com"), "escapeGroup.md");
+    public void testFileOne() throws IOException{
+        String contents = Files.readString(Path.of("./test1.md"));
+        ArrayList<String> testResults = new ArrayList<>();
+        testResults.add("google.com");
+        testResults.add("google.com");
+        testResults.add("ucsd.edu");
+        assertEquals(testResults, MarkdownParseGroup.getLinks(contents));
     }
 
     @Test
-    public void testJustEscape() throws IOException {
-        assertLinks(List.of(), "justEscapeGroup.md");
+    public void testFileTwo() throws IOException{
+        String contents = Files.readString(Path.of("./test2.md"));
+        ArrayList<String> testResults = new ArrayList<>();
+        testResults.add("a.com");
+        testResults.add("a.com(())");
+        testResults.add("example.com");
+        assertEquals(testResults, MarkdownParseGroup.getLinks(contents));
     }
 
     @Test
-    public void testImage() throws IOException {
-        assertLinks(List.of(), "imageGroup.md");
-    }
-
-    @Test
-    public void testJustBrackets() throws IOException {
-        assertLinks(List.of(), "justBracketsGroup.md");
-    }
-
-    @Test
-    public void testJustParentheses() throws IOException {
-        assertLinks(List.of(), "justParenthesesGroup.md");
-    }
-
-    @Test
-    public void testMultiline() throws IOException {
-        assertLinks(List.of("https://isthisfound.com"), "multilineGroup.md");
-    }
-
-    @Test
-    public void testLastLine() throws IOException {
-        assertLinks(List.of("last line link should be found"), "lastLineGroup.md");
-    }
-
-    public static void assertLinks(List<String> expectedLinks, String fileName) throws IOException {
-        Path filePath = Path.of(fileName);
-        String contents = Files.readString(filePath);
-        ArrayList<String> links = MarkdownParseGroup.getLinks(contents);
-
-        assertEquals(expectedLinks, links);
+    public void testFileThree() throws IOException{
+        String contents = Files.readString(Path.of("./test3.md"));
+        ArrayList<String> testResults = new ArrayList<>();
+        testResults.add("https://www.twitter.com");
+        testResults.add("https://ucsd-cse15l-w22.github.io/");
+        testResults.add("https://cse.ucsd.edu/");
+        assertEquals(testResults, MarkdownParseGroup.getLinks(contents));
     }
 }
